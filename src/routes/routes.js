3799d3796b22968ecb212080a58dfe7b8f9a2601
Vitória Maria <109771542+VihTech@users.noreply.github.com>
2023-04-19 11:09:56 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,6 @@ route.post('/', to_doController.create); //Post cadastra informações no BD
 route.get('/', to_doController.findAll); //Get pega informações no BD
 route.get('/:id',validId, validTo_do, to_doController.findById); //Get pega informações no BD com um parametro
 route.patch('/:id', validId, validTo_do, to_doController.update) // Patch atualiza um elemento por vez
-//route.get('/:nome', findByName)
+route.delete('/:id', validId, validTo_do, to_doController.remove) // Delete deleta um elemento pelo id
 
 export default route
