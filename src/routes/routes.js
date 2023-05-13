2909443548ee2042d46aab4eca4792233c5c0abc
VihTech <vitoriamaria.rodrigues02@gmail.com>
2023-05-13 19:30:25 -0300

    melhorias

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,8 +6,8 @@ import validTo_do from "../middlewares/global.middlewares.js"
 const route = Router()
 
 // O middlewares devem rodar antes de entrar no controller
-route.post('/', to_doController.create); //Post cadastra informações no BD
-route.get('/', to_doController.findAll); //Get pega informações no BD
+route.post('/to-do', to_doController.create); //Post cadastra informações no BD
+route.get('/to-do', to_doController.findAll); //Get pega informações no BD
 route.get('/:id',validId, validTo_do, to_doController.findById); //Get pega informações no BD com um parametro
 route.patch('/:id', validId, validTo_do, to_doController.update) // Patch atualiza um elemento por vez - usando post
 route.delete('/:id', validId, validTo_do, to_doController.remove) // Delete deleta um elemento pelo id
