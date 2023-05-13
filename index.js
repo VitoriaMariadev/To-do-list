10bd327d4ceafd13a09ca62838e63eaec83e7ef2
VihTech <vitoriamaria.rodrigues02@gmail.com>
2023-05-13 19:27:59 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 import express from "express";
 import to_doRoute from "./src/routes/routes.js"
 import connectDatabase from "./src/database/db.js";
+import bodyParser from "body-parser";
 import cors from 'cors'
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
 const app = express();
 const port = 3000
 
-
 // Caminho para acessar
 connectDatabase()
 app.use(express.json());
-app.use("/to_do", to_doRoute)
+app.use("/", to_doRoute)
 
-to_doRoute.get('/', (req, res) => {
-    return res.json({
-        sucess: true,
-        message: "sucesso"
-    })
-    
-})
-
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use(cors())
+connectDatabase()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use("/", to_doRoute)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
-app.use(cors())
